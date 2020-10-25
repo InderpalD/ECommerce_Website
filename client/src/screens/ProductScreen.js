8016a6b5e2ea9e5b85f3f3227a42f93931fc078e
InderpalD <isdhillo@ucsd.edu>
2020-10-25 13:24:48 -0400

    starting server

---
+++ client/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
+import Rating from '../components/Rating'
+function ProductScreen({match}) {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default ProductScreen
