927c51963fa6b7694601e7682eee6a133a110932
InderpalD <isdhillo@ucsd.edu>
2020-11-17 18:01:29 -0500

    added product routes to server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express  from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 dotenv.config()
 connectDB()
 const app = express()
@@ -10,16 +12,14 @@ app.get('/', (req, res) => {
   res.send('api is running')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+
 
 const PORT = process.env.PORT || 5000
+
 
 app.listen(
   PORT,
